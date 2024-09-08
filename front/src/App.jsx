c3149217c19d8fd0165c1aa7ed0ad4ca3394b3fb
leen7awa <leen.hawa@e.braude.ac.il>
2024-09-08 17:04:56 +0300

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -40,7 +40,8 @@ function App() {
 
   if (localStorage.auth !== undefined) {
     const token = JSON.parse(localStorage.auth);
-    axios.defaults.baseURL = 'http://localhost:3000';
+    // axios.defaults.baseURL = 'http://localhost:3000';
+    axios.defaults.baseURL = 'https://blockchain-gilt.vercel.app/';
     axios.defaults.headers.common['authorization'] = `Bearer ${token}`;
   }
 
